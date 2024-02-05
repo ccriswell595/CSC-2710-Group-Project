e44c5d1b4d153740ed6253dd832a231859e74175
ccriswell595 <159051764+ccriswell595@users.noreply.github.com>
2024-02-05 11:43:09 -0500

    Update maintest.cpp

--- maintest.cpp
+++ maintest.cpp
@@ -1,1 +1,20 @@
+/********************************************************
+* Colton Criswell, 
+* Course: CSC2710-01
+* Date: 
+* Description:
+**********************************************************/
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+using namespace std;
 
+
+
+int main()
+{
+
+
+
+  return 0;
+}
