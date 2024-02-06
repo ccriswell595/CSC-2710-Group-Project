d99e30cb32efdb39a56990301b486971bdf94c3b
lcdoyle8 <159052317+lcdoyle8@users.noreply.github.com>
2024-02-06 15:23:01 -0500

    Update mergesort.cpp

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,3 +1,18 @@
 /*
-
+Liz Doyle
 */
+#include <iostream>
+
+void mergesort (int n, S[])
+{
+  if (n>1)
+  {
+    const int h = [ n/2 ], m = n - h;
+    U[ 1 .. h ], V[ 1 .. m ];
+    copy S[1] through S[h] to U[1] through U[h];
+    copy S[ h + 1] through S[n] to V[1] through V[m];
+    mergesort(h, U);
+    mergesort(m, V);
+    merge( h, m, U, V, S );
+  }
+}
