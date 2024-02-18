31b41dc8af4085a135192a03d7ddf16001dad6a0
ccriswell595 <159051764+ccriswell595@users.noreply.github.com>
2024-02-18 18:47:50 -0500

    Create sortingAlgorithms.h

---
+++ sortingAlgorithms.h
@@ -0,0 +1,11 @@
+#ifndef SORTING_ALGORITHMS_H
+#define SORTING_ALGORITHMS_H
+
+void selectionSort(int arr[], int length);
+void bubbleSort(int arr[], int length);
+void insertionSort(int arr[], int length);
+void mergeSort(int arr[], int left, int right);
+void quickSort(int arr[], int low, int high);
+void heapSort(int arr[], int length);
+
+#endif 
