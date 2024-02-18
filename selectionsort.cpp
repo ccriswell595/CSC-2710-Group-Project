5eb012289851eb9931072074b764830eef9082f7
aliya <91815922+AGfegg@users.noreply.github.com>
2024-02-18 18:36:36 -0500

    Update selectionsort.cpp

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -1,1 +1,147 @@
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <chrono>
+using namespace std;
+void selectionSort(int n, int s[]);
+void randomArray(int n, int s[]);
+void nearlySorted(int n,int ne[]);
+void reversed(int n, int re[]);
+void fewUnique(int n, int fe[]);
 
+int main()
+{
+
+int n=10;
+int s[n];
+randomArray(n,s);
+int ne[n]= {1,2,4,3,6,7,5,8,9,0};
+nearlySorted(n, ne);
+int re[n] ={0,9,8,7,6,5,4,3,2,1};
+reversed(n, ne);
+n=12;
+int fe[n]= {3,4,3,4,5,7,6,8,6,5,6,8};
+fewUnique(n, fe);
+
+return 0;
+}
+//random array initialize aand sort
+void randomArray(int n, int s[])
+{
+//fill array with random
+for ( int a = 0; a<n; a++)
+   {int r = rand()%1000;
+     s[a]=r;
+   }
+
+int i;
+//output initial array
+for (i=0; i<n; i++)
+   cout<<s[i]<<endl;
+
+//time start
+auto start = std::chrono::system_clock::now();
+
+//selection sort call
+selectionSort(n, s);
+
+//time end
+auto end = std::chrono::system_clock::now();
+
+for (i=0; i<n; i++)
+  cout<<s[i]<<endl;
+
+std::chrono::duration<double> elapsed_seconds = end-start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+    std::cout << "sort time for random: " << elapsed_seconds.count() << "s"
+              << std::endl;
+
+}
+//nearly sorted array and call sort
+void nearlySorted(int n, int ne[])
+{
+//time start
+auto start = std::chrono::system_clock::now();
+
+//selection sort call
+selectionSort(n, ne);
+
+//time end
+auto end = std::chrono::system_clock::now();
+
+//for (i=0; i<n; i++)
+ // cout<<s[i]<<endl;
+
+std::chrono::duration<double> elapsed_seconds = end-start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+    std::cout << "sort time for nearly Sorted array: " << elapsed_seconds.count() << "s"
+              << std::endl;
+}
+
+
+//reverseres sorted
+void reversed(int n, int re[])
+{
+//time start
+auto start = std::chrono::system_clock::now();
+
+//selection sort call
+selectionSort(n, re);
+
+//time end
+auto end = std::chrono::system_clock::now();
+
+//for (i=0; i<n; i++)
+  //cout<<s[i]<<endl;
+
+std::chrono::duration<double> elapsed_seconds = end-start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+    std::cout << "sort time for reversed: " << elapsed_seconds.count() << "s"
+              << std::endl;
+
+}
+
+void fewUnique(int n, int fe[])
+{
+
+//time start
+auto start = std::chrono::system_clock::now();
+
+//selection sort call
+selectionSort(n, fe);
+
+//time end
+auto end = std::chrono::system_clock::now();
+
+//for (i=0; i<n; i++)
+  //cout<<s[i]<<endl;
+
+std::chrono::duration<double> elapsed_seconds = end-start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+    std::cout << "sort time for few unique: " << elapsed_seconds.count() << "s"
+              << std::endl;
+}
+//selection sort code
+void selectionSort(int n, int s[])
+{
+
+int temp;
+int i, j, smallest;
+for(i=1; i<=n-2; i++)
+ {
+   smallest=i;
+   for(j=i+1; j<=n-1; j++)
+       if (s[j]<s[smallest])
+          smallest=j;
+
+     temp = s[i];
+     s[i] = s[smallest];
+     s[smallest] = temp;
+
+ }
+
+}
