4449529c233808451021808c4abe749c4e137b01
ccriswell595 <159051764+ccriswell595@users.noreply.github.com>
2024-02-18 20:11:02 -0500

    Update sortingAlgorithms.h

--- sortingAlgorithms.h
+++ sortingAlgorithms.h
@@ -1,11 +1,11 @@
 #ifndef SORTING_ALGORITHMS_H
 #define SORTING_ALGORITHMS_H
 
-void selectionSort(int arr[], int length);
-void bubbleSort(int arr[], int length);
-void insertionSort(int arr[], int length);
-void mergeSort(int arr[], int left, int right);
-void quickSort(int arr[], int low, int high);
-void heapSort(int arr[], int length);
+//void selectionSort(int arr[], int length);
+//void bubbleSort(int arr[], int length);
+//void insertionSort(int arr[], int length);
+//void mergeSort(int arr[], int left, int right);
+int quicksort(int arr[], int low, int length);
+//void heapSort(int arr[], int length);
 
 #endif 
