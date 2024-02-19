483c490699c93a6f364a32175550642f0403e052
hannahbush <99496798+hannahbush@users.noreply.github.com>
2024-02-18 23:04:15 -0500

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -12,6 +12,8 @@
 
 
 void bubbleSort(int arr[], int n) {
+   auto start = high_resolution_clock::now(); //time start
+
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
