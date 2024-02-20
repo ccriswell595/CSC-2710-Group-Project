f5b64b32754c8211f99a326e46c02db3504c0497
lcdoyle8 <159052317+lcdoyle8@users.noreply.github.com>
2024-02-20 13:04:50 -0500

    Update sortingAlgorithms.h

--- sortingAlgorithms.h
+++ sortingAlgorithms.h
@@ -4,7 +4,7 @@
 void selectionsort(int arr[], int length);
 //void bubbleSort(int arr[], int length);
 //void insertionSort(int arr[], int length);
-//void mergeSort(int arr[], int left, int right);
+void mergesort(int arr[], int start, int end);
 void quicksort(int arr[], int low, int high);
 //void heapSort(int arr[], int length);
 
