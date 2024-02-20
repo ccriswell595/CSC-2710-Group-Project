ca99517473017be0f04b973311f3f7237eb01640
lcdoyle8 <159052317+lcdoyle8@users.noreply.github.com>
2024-02-20 14:49:06 -0500

    Update maintest.cpp

--- maintest.cpp
+++ maintest.cpp
@@ -193,3 +193,70 @@ void manyDuplicates(int* arr, int length) //essentially uses modulo and rand() t
     }
 }
 
+void merge(int* arr, int start, int mid, int end)
+{   
+    int leftPtr = start; 
+    int rightPtr = mid + 1; 
+    int mergedArrSize = end - start + 1;
+    int mergedArray[mergedArrSize];
+    
+    int i = 0;
+    while(leftPtr <= mid && rightPtr <= end)
+    {   
+        if(arr[leftPtr] <= arr[rightPtr])
+        {   
+            mergedArray[i] = arr[leftPtr];
+            leftPtr++;
+        }
+        else
+        {   
+            mergedArray[i] = arr[rightPtr];
+            rightPtr++;
+        }
+        i++;
+    }
+    
+    while(leftPtr <= mid)
+    {   
+        mergedArray[i] = arr[leftPtr];
+        leftPtr += 1;
+        i++;
+    }
+    
+    while(rightPtr <= end)
+    {   
+        mergedArray[i] = arr[rightPtr];
+        rightPtr += 1;
+        i++;
+    }
+    
+    for (int j = 0; j < mergedArrSize; j++)
+    {   
+        arr[start + j] = mergedArray[j];
+    }
+
+}
+
+void mergesort (int arr[], int start, int end)
+{
+  if (start < end)
+  {
+    int mid = (start + end)/2;
+    int lower[mid];
+    int upper[end];
+
+    for (int i = 0; i < mid; i++)
+    {
+      lower[i] = arr[i];
+    }
+
+    for(int j = mid - 1; j < end; j++)
+    {
+      upper[j] = arr[j];
+    }
+
+    mergesort(arr, start, mid);
+    mergesort(arr, mid + 1, end);
+    merge(arr, start, mid, end);
+  }
+}
