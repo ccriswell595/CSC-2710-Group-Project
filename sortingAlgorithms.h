7c8953bf543687b64ad65ddabd6990b4207fb55f
ccriswell595 <159051764+ccriswell595@users.noreply.github.com>
2024-02-21 00:07:58 -0500

    Update sortingAlgorithms.h

--- sortingAlgorithms.h
+++ sortingAlgorithms.h
@@ -6,6 +6,11 @@ void bubbleSort(int arr[], int length);
 //void insertionSort(int arr[], int length);
 void mergesort(int arr[], int start, int end);
 void quicksort(int arr[], int low, int high);
-//void heapSort(int arr[], int length);
+struct heap {
+    int *S;
+    int heapsize;
+};
+void heapsort(int n, heap& H, int S[]);
+
 
 #endif 
