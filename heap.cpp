74e4393d537f383c4eb3ee9a556329bd35ca0dec
ccriswell595 <159051764+ccriswell595@users.noreply.github.com>
2024-02-21 00:05:51 -0500

    Update heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -1,69 +1,66 @@
+#include <iostream>
+#include "sortingAlgorithms.h"
 
+typedef int keytype;
 
+void heapsort(int n, heap& H, keytype S[]);
+void removekeys(int n, heap& H, keytype S[]);
+void makeheap(int n, heap& H);
+keytype root(heap& H);
+void siftdown(heap& H, int i);
 
-
-void heapsort (int n, heap H, const S[])
+void heapsort(int n, heap& H, keytype S[])
 {
+    H.S = S;
     makeheap(n, H);
     removekeys(n, H, S);
 }
 
-void removekeys(int n, heap& H, const S[])
+void removekeys(int n, heap& H, keytype S[])
 {
-    index i;
-    for(i=n; i>= 1; i--)
+    int i;
+    for (i = n; i > 0; i--)
         S[i] = root(H);
 }
 
 void makeheap(int n, heap& H)
 {
-    index i;
-
+    int i;
     H.heapsize = n;
-    for(i = [n/2]; i>=1; i--)
+    for (i = n / 2; i >= 0; i--)
         siftdown(H, i);
 }
 
-keytype root(heap&, H)
+keytype root(heap& H)
 {
-    keytype keyout;
-    keyout = key;
-    //move key at the bottom of the node to the root
-    // delete the bottom node
-    siftdown(H);
+    keytype keyout = H.S[0];
+    H.S[0] = H.S[H.heapsize--];
+    siftdown(H, 0);
     return keyout;
 }
 
-void siftdown(heap& H)
+void siftdown(heap& H, int i)
 {
-    index parent, lchild;
+    int lchild;
     keytype siftkey;
     bool found;
 
     siftkey = H.S[i];
-    parent = i;
+    int parent = i;
     found = false;
-    
-    while(parent <= H.heapsize && ! found)
+
+    while (parent * 2 <= H.heapsize && !found)
     {
-        if(parent < H.heapsize && H.S[parent] < H.S[parent + 1])
-            lchild = parent + 1;
-        else
-            lchild = parent;
-        if(siftkey < H.S[lchild])
+        lchild = parent * 2;
+        if (lchild < H.heapsize && H.S[lchild] < H.S[lchild + 1])
+            lchild++;
+        if (siftkey < H.S[lchild])
         {
-            H.S[parent] = H.S[lchild]
-            parent = lchild
+            H.S[parent] = H.S[lchild];
+            parent = lchild;
         }
-        else 
-        found = true;
+        else
+            found = true;
     }
     H.S[parent] = siftkey;
 }
-
-struct heap
-{
-    const S[1..n];
-    int heapsize;
-}
-
