680c5c7853fcf799dc2a7aa93e41c84875fa1e1b
hannahbush <99496798+hannahbush@users.noreply.github.com>
2024-02-20 22:30:08 -0500

    Update sortingAlgorithms.h

--- sortingAlgorithms.h
+++ sortingAlgorithms.h
@@ -2,7 +2,7 @@
 #define SORTING_ALGORITHMS_H
 
 void selectionsort(int arr[], int length);
-//void bubbleSort(int arr[], int length);
+void bubbleSort(int arr[], int length);
 //void insertionSort(int arr[], int length);
 void mergesort(int arr[], int start, int end);
 void quicksort(int arr[], int low, int high);
