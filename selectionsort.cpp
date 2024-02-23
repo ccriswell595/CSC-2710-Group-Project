4ccf303f6229487f3abb8026b5b0f9bdb2e30537
chrismith26 <130072559+chrismith26@users.noreply.github.com>
2024-02-23 11:08:23 -0500

    Update selectionsort.cpp

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -14,23 +14,27 @@ int selectionsort(int s[], int n)
    //time start
  auto start = std::chrono::high_resolution_clock::now();
 int temp;
+int count = 0;
 int i, j, smallest;
 for(i=1; i<=n-2; i++)
  {
    smallest=i;
    for(j=i+1; j<=n-1; j++)
+   {
+      count++;
        if (s[j]<s[smallest])
           smallest=j;
-
+   
      temp = s[i];
      s[i] = s[smallest];
      s[smallest] = temp;
+   }
  }
   //time end
 auto end = std::chrono::high_resolution_clock::now(); 
 std::chrono::nanoseconds elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
     int time= elapsed.count();
-
+cout << "comepares: " << count << endl;
    return time;
  }
    
