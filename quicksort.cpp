e37e78c2f8852e0fd67a9068f323d3d7ae95741e
lcdoyle8 <159052317+lcdoyle8@users.noreply.github.com>
2024-02-23 11:36:11 -0500

    Update quicksort.cpp

--- quicksort.cpp
+++ quicksort.cpp
@@ -6,17 +6,20 @@
 void partition(int arr[], int low, int high, int* pivot); 
 void quicksort(int arr[], int low, int high)
 {
+    int counter = 0;
     if (low < high) {
         int pivot;
         partition(arr, low, high, &pivot); 
         quicksort(arr, low, pivot - 1);
         quicksort(arr, pivot + 1, high);
+        counter++;
     }
     return; 
 }
 
 void partition(int arr[], int low, int high, int* pivot) 
 {
+    int counter = 0;
     int mid = low + (high - low) / 2;
     int pivotIndex = (arr[low] < arr[mid]) ? //use conditional logic to find the median of the 3 points
                      ((arr[mid] < arr[high]) ? mid : ((arr[low] < arr[high]) ? high : low)):
@@ -34,9 +37,11 @@ void partition(int arr[], int low, int high, int* pivot)
     {
         while (i <= j && arr[i] <= pivotVal) {
             i++;
+            counter++;
         }
         while (i <= j && arr[j] > pivotVal) {
             j--;
+            counter++;
         }
         if (i >= j) {
             break;
